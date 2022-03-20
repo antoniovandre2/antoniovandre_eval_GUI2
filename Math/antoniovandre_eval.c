ebd4579d977b1e87def985387598d42eb8e4130b
Antonio Vandré Pedrosa Furtunato Gomes <90938909+antoniovandre2@users.noreply.github.com>
2022-03-19 22:47:12 -0300

    Create antoniovandre_eval.c

---
+++ Math/antoniovandre_eval.c
@@ -0,0 +1,61 @@
+// Projeto Mathematical Ramblings (mathematicalramblings.blogspot.com).
+
+// Retorna a avaliação (eval) da expressão passada como argumento, versão de distribuição.
+
+// Licença de uso: Atribuição-NãoComercial-CompartilhaIgual (CC BY-NC-SA).
+
+// Última atualização: 12-07-2020.
+
+#include "../antoniovandre.c"
+
+#define EVAL_MENSAGEM_USO "Use antoniovandre_eval <EXPRESSÃO VÁLIDA>."
+#define CABECALHO_ESTATISTICAS_MATHEVAL "eval"
+
+int main (int argc, char *argv [])
+	{
+	char * strf;
+	char * argvb;
+	int i;
+	int j;
+
+	argvb = (char *) malloc (TAMANHO_BUFFER_PHRASE);
+
+	strcpy (argvb, argv [1]);
+
+	if (argc == 1 || argc > 2 || (argc == 2 && (! strcmp (argv [1], "h") || ! strcmp (argv [1], "help") || ! strcmp (argv [1], "-h") || ! strcmp (argv [1], "--help"))))
+		{
+		printf (EVAL_MENSAGEM_USO);
+		printf ("\n");
+		return -1;
+		}
+
+	if (argc == 2 && (! strcmp (argv [1], "sobre") || ! strcmp (argv [1], "--sobre")))
+		{
+		if (! antoniovandre_mathsobre ()) return -1;
+		return 0;
+		}
+
+	strf = (char *) malloc (TAMANHO_BUFFER_PHRASE);
+
+	strf = antoniovandre_eval (argvb);
+
+	if (! strcmp (strf, STRINGSAIDAERRO))
+		{
+		printf (EVAL_MENSAGEM_USO);
+		printf ("\n");
+		return -1;
+		}
+	else if (! strcmp (strf, STRINGSAIDAERROOVER))
+		{
+		printf (MENSAGEM_ERRO_OVER);
+		printf ("\n");
+		return -1;
+		}
+	else
+		printf ("%s\n", strf);
+
+	if (! antoniovandre_salvarmathestatisticas (CABECALHO_ESTATISTICAS_MATHEVAL)) return -1;
+
+	return 0;
+	}
+
